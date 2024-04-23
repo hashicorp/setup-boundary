915fa426d623bad8d67ff80207c262a1bc5456c9
Bruno Schaatsbergen <git@bschaatsbergen.com>
2024-04-23 19:37:25 +0200

    feat: initial release

---
+++ src/index.ts
@@ -0,0 +1,26 @@
+/**
+ * Copyright (c) HashiCorp, Inc.
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+import * as core from "@actions/core";
+import * as hc from "@hashicorp/github-actions-core";
+
+export const PRODUCT = "boundary";
+
+async function main() {
+  const version = core.getInput("version");
+  if (version == null) {
+    core.setFailed("Required field 'version' not set");
+    return;
+  }
+  try {
+    await hc.getHashicorpRelease(PRODUCT, version);
+  } catch (error) {
+    if (error instanceof Error) {
+      core.setFailed(error.message);
+    }
+  }
+}
+
+main();
